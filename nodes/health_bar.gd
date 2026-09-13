635b6648b807fbf1174f1916ae57902e68cdf230
LeX <pierre@lastexile.fr>
2026-09-14 00:42:14 +0200

    funnier shader

--- nodes/health_bar.gd
+++ nodes/health_bar.gd
@@ -6,19 +6,20 @@ var health_lost: float
 
 @onready var mat: Material = material
 
+
 func setup(health: int):
 	health_lost = 0
 
-	mat.set_shader_parameter("energy_total", health)
-	mat.set_shader_parameter("energy_left", health)
-	mat.set_shader_parameter("energy_lost", health_lost)
+	set_shader_total(health)
+	set_shader_left(health)
+	set_shader_lost(health_lost)
 
 
 func lose_health(amount: int, health: int) -> void:
 	health_lost += amount if health >= 0 else amount + health
 	
-	mat.set_shader_parameter("energy_left", max(health, 0))
-	mat.set_shader_parameter("energy_lost", health_lost)
+	set_shader_left(max(health, 0))
+	set_shader_lost(health_lost)
 
 	if tween:
 		tween.kill()
@@ -26,7 +27,20 @@ func lose_health(amount: int, health: int) -> void:
 	tween.tween_interval(0.5)
 	tween.tween_method(
 		func(val: float):
-			mat.set_shader_parameter("energy_lost", val)
+			set_shader_lost(val)
 			health_lost = val,
 		health_lost, 0, 2
 	)
+
+
+func set_shader_param(param: String, value: float) -> void:
+	mat.set_shader_parameter(param, value)
+
+func set_shader_total(value: float) -> void:
+	mat.set_shader_parameter("energy_total", value)
+
+func set_shader_left(value: float) -> void:
+	mat.set_shader_parameter("energy_left", value)
+
+func set_shader_lost(value: float) -> void:
+	mat.set_shader_parameter("energy_lost", value)
